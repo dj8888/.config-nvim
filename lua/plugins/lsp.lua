bb2476a3e6fb016c21cbb4dd966e7d83a8c3fe3c
Devansh <devansh.jyoti8888@gmai.com>
2026-03-20 14:11:42 +0530

    Removed duplicate binding.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,9 +108,6 @@ return {
 					-- or a suggestion from your LSP for this to activate.
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
-					-- Show LSP/diagnostic message for the current line (floating window).
-					map("<leader>e", vim.diagnostic.open_float, "Show line [E]rror / diagnostic message")
-
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
